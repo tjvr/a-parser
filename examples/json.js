7101170d301e5fba885ca20d5daad07185324b70
Tim Radvan <tim@tjvr.org>
2019-01-04 21:17:33 +0000

    Add JSON example

---
+++ examples/json.js
@@ -0,0 +1,74 @@
+const moo = require("moo")
+
+const newGrammar = require("../grammar").newGrammar
+
+const lexer = moo.compile({
+  space: { match: /\s+/, lineBreaks: true },
+  NUMBER: /-?(?:[0-9]|[1-9][0-9]+)(?:\.[0-9]+)?(?:[eE][-+]?[0-9]+)?\b/,
+  STRING: {
+    match: /"(?:\\["bfnrt\/\\]|\\u[a-fA-F0-9]{4}|[^"\\])*"/,
+    value: x => JSON.parse(x),
+  },
+  "{": "{",
+  "}": "}",
+  "[": "[",
+  "]": "]",
+  ",": ",",
+  ":": ":",
+  TRUE: /true\b/,
+  FALSE: /false\b/,
+  NULL: /null\b/,
+})
+
+const grammar = newGrammar(`
+
+json Object -> "{" items:items "}"
+json Array -> "[" items:array "]"
+json String -> value:"STRING"
+json Number -> value:"NUMBER"
+json Bool   -> value:"TRUE"
+json Bool   -> value:"FALSE"
+json Null   -> "NULL"
+
+items [] -> []:items "," :item
+items [] -> :item
+items [] ->
+
+array [] -> []:array "," :json
+array [] -> :json
+array [] ->
+
+item Item -> key:"STRING" ":" value:json
+
+`)
+
+function process(node) {
+  switch (node.type) {
+    case "Object":
+      const d = {}
+      for (let item of node.items) {
+        d[item.key] = process(item.value)
+      }
+      return d
+    case "Array":
+      const a = []
+      for (let item of node.items) {
+        a.push(process(item))
+      }
+      return a
+    case "Number":
+      return +node.value
+    case "String":
+      return node.value
+    case "Bool":
+      return node.value === "true"
+    case "Null":
+      return null
+  }
+}
+
+module.exports = {
+  lexer,
+  grammar,
+  process,
+}
