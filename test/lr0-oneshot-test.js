e30d31523c8c8e28fd0da69dfd2c2cff524a86b1
Tim Radvan <tim@tjvr.org>
2022-11-30 00:16:18 +0000

    WIP oneshot tests

---
+++ test/lr0-oneshot-test.js
@@ -0,0 +1,64 @@
+const fs = require("fs")
+
+const test = require("ava")
+
+const grammar = require("../grammar")
+const compile = require("../lr0-oneshot")
+
+test("json", (t) => {
+  const { lexer, grammar, process } = require("../examples/json")
+
+  const parser = compile(grammar)
+
+  const source = `
+  {
+    "foo": [1, 2], "bar": {"twenty-four": null},
+  "cows": true, "no": false, "cow": "cow\\n\\n"}`
+
+  lexer.reset(source)
+  let tok
+  try {
+    const tree = parser.parse(() => {
+      while ((tok = lexer.next())) {
+        if (tok.type === "space") {
+          continue
+        }
+        return tok
+      }
+    })
+  } catch (err) {
+    t.fail(lexer.formatError(tok, err.message))
+  }
+
+  t.deepEqual(process(tree), JSON.parse(source))
+})
+
+test("json benchmark", (t) => {
+  const { lexer, grammar, process } = require("../examples/json")
+  const parser = compile(grammar)
+
+  let jsonFile = fs.readFileSync("benchmark/json/sample1k.json", "utf-8")
+
+  lexer.reset(jsonFile)
+  parser.reset()
+  while ((tok = lexer.next())) {
+    if (tok.type === "space") continue
+    parser.eat(tok)
+  }
+  const tree = parser.result()
+
+  t.deepEqual(process(tree), JSON.parse(jsonFile))
+})
+
+test("brackets", (t) => {
+  const { lexer, grammar, process } = require("../examples/brackets")
+  const parser = compile(grammar)
+
+  lexer.reset(`( a ; b ; c )`)
+  let tok
+  while ((tok = lexer.next())) {
+    if (tok.type === "space") continue
+    parser.eat(tok)
+  }
+  t.deepEqual(process(parser.result()), [["a", "b"], "c"])
+})
