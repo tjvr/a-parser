65ceafd65805c55296bd1cea0b5db12d16f07bb5
Tim Radvan <tim@tjvr.org>
2022-11-30 00:21:56 +0000

    benchmark lr0-oneshot

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -10,6 +10,9 @@ suite("json", () => {
   const compileLR0 = require("../lr0")
   const lr0Parser = compileLR0(grammar)
 
+  const compileLR0Oneshot = require("../lr0-oneshot")
+  const lr0OneshotParser = compileLR0Oneshot(grammar)
+
   let jsonFile = fs.readFileSync("benchmark/json/sample1k.json", "utf-8")
 
   benchmark("nearley", () => {
@@ -31,6 +34,18 @@ suite("json", () => {
       lr0Parser.eat(tok)
     }
     const tree = lr0Parser.result()
+    // process(tree)
+  })
+
+  benchmark("lr0-oneshot", () => {
+    lexer.reset(jsonFile)
+    const tree = lr0OneshotParser.parse(() => {
+      let tok
+      while ((tok = lexer.next())) {
+        if (tok.type === "space") continue
+        return tok
+      }
+    })
     // process(tree)
   })
 
